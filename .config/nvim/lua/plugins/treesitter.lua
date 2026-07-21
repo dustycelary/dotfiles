bdcc4eb79af220269f742b52cbdf22f911ac1e4a
dustycelary <fungussh@gmail.com>
2026-07-21 10:26:04 +0100

    improving treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,71 +1,9 @@
--- return {
--- 	"nvim-treesitter/nvim-treesitter",
--- 	event = { "BufReadPre", "BufNewFile" },
--- 	branch = "master", -- The 'main' branch is a full rewrite that removes incremental selection.
--- 	build = ":TSUpdate",
--- 	config = function()
--- 		local configs = require("nvim-treesitter.configs")
---
--- 		-- configure treesitter
--- 		configs.setup({
--- 			-- ensure these language parsers are installed
--- 			ensure_installed = {
--- 				"json",
--- 				"javascript",
--- 				"typescript",
--- 				"tsx",
--- 				"yaml",
--- 				"html",
--- 				"css",
--- 				"prisma",
--- 				"markdown",
--- 				"markdown_inline",
--- 				"svelte",
--- 				"graphql",
--- 				"bash",
--- 				"lua",
--- 				"vim",
--- 				"dockerfile",
--- 				"gitignore",
--- 				"query",
--- 				"vimdoc",
--- 				"c",
--- 				"python",
--- 			},
---
--- 			-- enable syntax highlighting
--- 			highlight = {
--- 				enable = true,
--- 			},
--- 			-- enable indentation
--- 			indent = { enable = true },
---
--- 			incremental_selection = {
--- 				enable = true,
--- 				keymaps = {
--- 					-- NOTE: <C-CR> might not work in many terminals as they don't distinguish it from <CR>.
--- 					-- If it still doesn't work, consider changing these to something like "<C-Space>" or "grn".
--- 					init_selection = "<C-CR>",
--- 					node_incremental = "<C-CR>",
--- 					scope_incremental = false,
--- 					node_decremental = "<bs>",
--- 				},
--- 			},
--- 		})
---
--- 		-- use bash parser for zsh files
--- 		vim.treesitter.language.register("bash", "zsh")
--- 		vim.treesitter.language.register("bash", "conf")
--- 		vim.treesitter.language.register("bash", "env")
--- 	end,
--- }
---
-
 return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	branch = "main", -- Required for Neovim 0.12+
 	build = ":TSUpdate",
+	dependencies = { "shushtain/incselect.nvim" },
 	config = function()
 		require("nvim-treesitter").setup()
 
@@ -87,16 +25,24 @@ return {
 			"python",
 		})
 
-		-- enable highlighting
+		-- enable highlighting and indenting
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "*",
 			callback = function(args)
 				local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
 				if lang then
+					-- Enables highlighting
 					pcall(vim.treesitter.start, args.buf, lang)
+					-- Enables indenting
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 				end
 			end,
 		})
+
+		-- set up incremental selection
+		vim.keymap.set("n", "<C-CR>", require("incselect").init)
+		vim.keymap.set("x", "<C-CR>", require("incselect").parent)
+		vim.keymap.set("x", "<bs>", require("incselect").child)
 
 		-- use bash parser for zsh files
 		vim.treesitter.language.register("bash", "zsh")
